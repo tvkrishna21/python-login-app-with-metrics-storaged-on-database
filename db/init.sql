27b26c3a6f70e27dfe06e82a221cc6842b81316b
tvkrishna21 <76438536+tvkrishna21@users.noreply.github.com>
2021-12-23 19:40:06 +0530

    Add files via upload

---
+++ db/init.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE custmet;
+use custmet;
+
+CREATE TABLE metrics (
+  mname VARCHAR(20),
+  mtype INT
+);
+
+#INSERT INTO metrics
+#  (mname, mtype)
+#VALUES
+#  ('Login', 0),
+#  ('Successful', 0),
+#	  ('Failed', 0);
+
+
